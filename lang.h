17ac6686411c7a5c5ad124662dfb55e64d9dd9ba
Matías Israelson <57065102+israpps@users.noreply.github.com>
2021-05-04 23:29:32 -0300

    Update lang.h

--- lang.h
+++ lang.h
@@ -1,13 +1,13 @@
 #ifdef LANG_SPA
 #define CUSTOM_LNG
-#include "Lang/SPA.LNG"
+#include "lang/SPA.LNG"
 #endif
 
 #ifdef LANG_ENG
 #define CUSTOM_LNG
-#include "Lang/ENG.LNG"
+#include "lang/ENG.LNG"
 #endif
 
 #ifndef CUSTOM_LNG
-#include "Lang/ENG.LNG"
+#include "lang/ENG.LNG"
 #endif
